7838dfe8d6053ee059051f8d2a89f8628306da5c
bertold <bertold@mabl.com>
2019-06-11 17:41:58 -0500

    static cors test

---
+++ cors/pass.js
@@ -0,0 +1,15 @@
+function main() 
+{
+    console.log("main function");
+    console.log("ajax request to the resource which does not require cors enabled");
+    $.ajax
+    ({
+        dataType: "json",
+        url: "https://api.github.com",
+        success: function(data) 
+        {
+            console.log("log response on success");
+            console.log(data);
+        }
+    });
+}
